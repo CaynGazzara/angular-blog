8459d56ec82d19711d06953c1861e9762e9dddb2
Iago Gazzara <iago.gazzara@softtrade.com.br>
2023-10-18 23:24:24 -0300

    projeto save

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,34 @@
+export const dataFake = [
+{
+    "id":"1",
+    "title": "Especie inedita de lobo é vista no artíco",
+    "description":"Ele foi visto peranbulando na floresta de Akiwi no artico",
+    "photoCover":"https://tse2.mm.bing.net/th?id=OIP.fMoaMKKdYcJgD8OCVddSlAHaEX&pid=Api&P=0&h=180"
+
+},
+{
+    "id":"2",
+    "title": "Inverno cobre floresta por completo",
+    "description":"Floresta no norte toda coberta por neve apos dois dias fortes de neve",
+    "photoCover":"https://tse2.mm.bing.net/th?id=OIP.5HwfdrrfPLKyuqzPJhemsgHaEK&pid=Api&P=0&h=180"
+
+
+},
+{
+    "id":"3",
+    "title": "matilhas de lobos é vista proxima a cidade costeira",
+    "description":"É hoje que eu volto amanhã.. Em rio de piranhas, jacaré nada de costas.. O povo unido é gente pra caramba.. You say potatoe, I say starchy carbs. Por que 'tudo junto' se escreve separado e 'separado' se escreve tudo junto?.",
+    "photoCover":"https://www.mundoecologia.com.br/wp-content/gallery/matilha-de-lobos/Matilha-de-Lobos-1.jpg"
+
+
+},
+{
+    "id":"4",
+    "title": "Deusa da caça Artemis e seu vinculo com os lobos ",
+    "description":"",
+    "photoCover":"https://mitologiagrega.net.br/wp-content/uploads/2018/04/mitologia-grega-artemis-deusa-da-lua-1170x642.jpg"
+
+
+}
+
+]
